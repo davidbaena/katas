a3ec4f7d9027942f3c28cb2bbbd4e83e7b9c4302
david.baena <david.baena@bq.com>
2019-03-13 12:07:04 +0100

    Monoid kata done

--- src/main/java/functional/abstractions/monoid/MonoidKata.kt
+++ src/main/java/functional/abstractions/monoid/MonoidKata.kt
@@ -1,5 +1,14 @@
 package functional.abstractions.monoid
 
+import functional.abstractions.functor.Failure
+import functional.abstractions.functor.Success
 import functional.abstractions.functor.Try
+import functional.abstractions.functor.fold
 
-fun <A> Try<A>.plus(any1: Any, any2: Any): Any = throw NotImplementedError()
+fun <A> Try<A>.plus(aTry: Try<A>, fn: (A, A) -> A): Try<A> =
+        fold(transformFailure = { Failure(it) },
+                transformSuccess = { value ->
+                    aTry.fold(
+                            transformFailure = { Failure(it) },
+                            transformSuccess = { Success(fn(value, it)) })
+                })
